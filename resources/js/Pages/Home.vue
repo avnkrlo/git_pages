fdb43a11a0cc7dd44ba3d07c502950fb239b2a92
Aivan Karlo <aivang1029@gmail.com>
2025-02-22 19:39:03 +0400

    git push

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="text-2xl font-bold">Welcome to Laravel + Vue + Inertia.js</h1>
+        <p>This is a basic setup without scaffolding.</p>
+    </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <title>Laravel + Vue + Inertia</title>
+    @vite('resources/js/app.js')
+    @routes
+    @inertiaHead
+</head>
+<body>
+    @inertia
+</body>
+</html>
