c4c6a4471b91f617d8338e3eac4c610777c61836
Aivan Karlo <aivang1029@gmail.com>
2025-03-11 22:23:51 +0400

    Implement VueFlux

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,10 +1,12 @@
 <script setup>
     import Navbar from '../Components/Navbar.vue';
+    import Jumbotron from '../Components/Jumbotron.vue';
     import Footer from '../Components/Footer.vue';
 </script>
 
 <template>
     <Navbar/>
+    <Jumbotron/>
         <slot/>
     <Footer/>
 </template>
