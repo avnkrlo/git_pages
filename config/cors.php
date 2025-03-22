a2508e0ee9bc3ffde9370ffa8d95664ae7fbb125
AK <37541753+avnkrlo@users.noreply.github.com>
2025-03-22 13:42:36 +0400

    cors issue

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','auth/steam'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['http://localhost:5173'], // Change '*' to your frontend URL if needed
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
